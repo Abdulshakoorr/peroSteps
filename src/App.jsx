1082c1ccd0181e90d218a1a3b6c16dbbbdad12f1
Shakoorkhana <as4630425@gmail.com>
2022-10-18 03:13:05 +0500

    creat routes for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home/Home'
+import Profile from './Pages/Profile/Profile'
+import Offer from './Pages/Offers/Offer'
+import ForgotPassword from'./Pages/Sign/ForgotPassword'
+import SignIn from'./Pages/Sign/SignIn'
+import SignUp from'./Pages/Sign/SignUp'
 const App = () => {
   return (
-    <div className='text-5xl h-[100vh] bg-orange-500'>App</div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/Offers" element={<Offer />} />
+        <Route path="/profile" exact={true} element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-red-500">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a pariatur,
+        quasi velit dignissimos nesciunt laborum atque? Suscipit libero quis
+        tenetur assumenda quaerat ipsum, amet fugiat voluptatem, velit non
+        facere.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
